75c53d4eb53f5956b004025933a14baca7caaa00
appni <d.ubelni@gmail.com>
2026-02-18 22:00:09 -0500

    Update 02.py

--- talleres/02/02.py
+++ talleres/02/02.py
@@ -4,13 +4,12 @@ for _ in range (3):
     row = [int(x) for x in input().split(',')]
     game.append(row)
 points = [0,0,0]
-current = 3
 is_even = lambda x : x % 2 == 0
 for i in range(n):
-    current = current + 1 if current < 2 else 0
     round = [row[i] for row in game]
-    if is_even(round[current]) :
-        points[current] += 1 if is_even(sum(round)) else 0
-    else: 
-        points[current] += 1 if not is_even(sum(round)) else 0
+    sum_is_even = is_even(sum(round))
+    for index, val in enumerate(round):
+        points[index] += 1 if is_even(val) and sum_is_even else 0     
+        points[index] += 1 if not is_even(val) and not sum_is_even else 0     
+   
 print(f'SO:{points[0]}, LAR:{points[1]}, IS:{points[2]}')
